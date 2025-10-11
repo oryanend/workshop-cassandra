b18b8beb7b43b1e0218c3dcb1385c326a7dd1e46
oryanend <ryanyamim@gmail.com>
2025-10-11 20:00:29 -0300

    Initial Setup

---
+++ src/test/java/com/oryanend/workshopcassandra/WorkshopCassandraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oryanend.workshopcassandra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopCassandraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
