d35164b71cb9ac76c59c9fc078ce54d6c82e83fc
oryanend <ryanyamim@gmail.com>
2025-10-13 22:50:01 -0300

    POST department

--- src/main/java/com/oryanend/workshopcassandra/controllers/DepartmentController.java
+++ src/main/java/com/oryanend/workshopcassandra/controllers/DepartmentController.java
@@ -5,11 +5,10 @@ import com.oryanend.workshopcassandra.services.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -33,5 +32,12 @@ public class DepartmentController {
         return ResponseEntity.ok().body(dto);
     }
 
-
+    @Transactional
+    @PostMapping
+    public ResponseEntity<DepartmentDTO> insert(@RequestBody DepartmentDTO dto) {
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
 }
